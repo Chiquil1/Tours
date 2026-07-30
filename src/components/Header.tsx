2fdcf1406d49b60097bf375daf874df99a2188f1
Nestor <00856217@red.unid.mx>
2026-07-29 19:16:36 -0500

    Correccciones en header y Sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 const Header = () => {
     return (
-        <header> classname "bg-blue 700 text-white p-5 shadow"
-
-            <h1>class name "text-2xl font-bold"
-                Sistema de Gestion de Tours</h1>
+        <header className="bg-blue-700 text-white p-5 shadow">
+            <h1 className="text-2xl font-bold">
+                Sistema de Gestion de Tours
+            </h1>
         </header>
     );
 };
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,17 +1,17 @@
 const Sidebar = () => {
     return (
         <aside className="w-64 h-screen bg-gray-800 text-white min-h-screen p-4">
-            <h2 className="text-xl font-bold mb-5">
+          <h2 className="text-xl font-bold mb-5">
                 Menu
             </h2>
-            <ul className="space-4">
-                <li className="hover:text-400 cursor-pointer">
+            <ul className="space-y-4">
+                <li className="hover:text-gray-400 cursor-pointer">
                     Dashboard
-                </li >
-                <li className="hover:text-400 cursor-pointer">
+                </li>
+                <li className="hover:text-gray-400 cursor-pointer">
                     Tablas tours
-                </li >
-            </ul >
+                </li>
+            </ul>
         </aside>
     );
 };
